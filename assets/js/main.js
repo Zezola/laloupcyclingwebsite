3c16dab343f5036628426d931a9f760b3f9f8d35
Zezola <joseulisses03@gmail.com>
2021-02-28 01:27:42 -0300

    Adicionando seçao 'sobre nos' e produto mais vendido

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+/* Making the navbar slide to the side */
 const navSlide = () => {
     const burger = document.querySelector(".burger");
     const nav = document.querySelector('.nav__links');
@@ -6,5 +7,14 @@ const navSlide = () => {
         nav.classList.toggle('nav-active');
     })
 }
+var c = 0;
 
+const backgroundImageSlider = () => {
+    const arrow_left = document.querySelector("#left-arrow");
+    const arrow_right = document.querySelector("#right-arrow");
+    const currentBackgroundImg = document.querySelector("")    
+    
+}
+
+/* Calling the functions */
 navSlide();
